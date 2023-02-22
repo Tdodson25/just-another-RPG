07c78fe2395bc463fd0ed7f83123193ecf4d2282
Trent Dodson <dodson-7-7@outlook.com>
2023-02-22 13:21:26 -0500

    player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,8 +4,6 @@ jest.mock('../lib/Potion');
 
 
 
-
-
 const Player = require('../lib/Player');
 
 jest.mock('../lib/Potion');
@@ -21,6 +19,27 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     )
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
 
 
 
